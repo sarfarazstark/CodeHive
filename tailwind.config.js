39905e73c4359ba6704ce18ac3521b000250f8bc
sarfarazstark <sarfarazstark@gmail.com>
2023-12-01 15:14:40 +0530

    Add new logo, update settings, and extend theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "moss-green": "hsla(70, 82%, 20%, 1)",
+        "apple-green": "hsla(63, 95%, 36%, 1)",
+        "hunyadi-yellow": "hsla(38, 89%, 60%, 1)",
+        "wheat": "hsla(35, 94%, 88%, 1)",
+      }
+    },
   },
-  plugins: [],
+plugins: [],
 }
 
