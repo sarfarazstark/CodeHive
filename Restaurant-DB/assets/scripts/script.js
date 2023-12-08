6a97f1ba19fa41956de62c15eb9c2cc432190ba3
sarfarazstark <sarfarazstark@gmail.com>
2023-12-08 19:14:07 +0530

    Add team page and update team section

--- Restaurant-DB/assets/scripts/script.js
+++ Restaurant-DB/assets/scripts/script.js
@@ -1,6 +1,5 @@
 import data from './data.json' assert { type: 'json' };
 import reviews from './reviews.json' assert { type: 'json' };
-import team from './team.json' assert { type: 'json' };
 import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js'
 console.log(data.restaurantName);
 
@@ -115,31 +114,4 @@ outlets.forEach(element => {
   </div>
 </div>`
   outletContainer.insertAdjacentHTML('afterbegin', html);
-})
-
-const card = document.querySelector('.cards')
-team.member.forEach(el => {
-  let links = [];
-  el.links.forEach(el => {
-    links = links + el;
-  })
-  console.log(links);
-  const html =
-`<div class="bg-white w-[350px] md:last:col-start-[-3] h-auto p-4 rounded-lg flex flex-col">
-  <div class="flex flex-col items-center mb-6">
-    <div class="w-full flex items-center h-24 rounded-lg ${el.gradient}"></div>
-    <img src="${el.image}" alt="${el.name}"
-      class="w-28 rounded-full -mt-16 border-4 border-moss-green">
-  </div>
-  <h1 class="text-moss-green text-center font-bold text-2xl">${el.name}</h1>
-  <p class="text-center text-gray-500 font-bold">${el.role}</p>
-  <div class="flex justify-center mt-8">
-  <a  href="mailto:" class="h-auto w-auto rounded-2xl ${el.gradient} p-1">
-    <span class="rounded-xl  flex h-full w-full items-center justify-center bg-gray-800 px-8 py-3 text-white">
-      EMAIL
-    </a>
-  </div>
-  <div class="flex justify-center gap-4 mt-6">${links}</div>
-</div>`;
-  card.insertAdjacentHTML('afterbegin', html);
 })
